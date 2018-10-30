12484d1b83d62783bde53ade69b8ec7523275129
RyanJF <ryanjfreeman77@gmail.com>
2018-10-30 18:24:41 -0500

    update sql file

--- bamazon_DB_seed.sql
+++ bamazon_DB_seed.sql
@@ -4,10 +4,13 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
+DROP TABLE IF EXISTS products;
+
 CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(50) NOT NULL,
-    department_name VARCHAR(50),
-    price DECIMAL(10 , 2 ) NOT NULL,
-    stock_quantity INT NOT NULL
-);
+	 item_id INT NOT NULL Auto_INCREMENT PRIMARY KEY
+    , product_name VARCHAR(50) NOT NULL
+    , department_name VARCHAR(50)
+    , unit_cost DECIMAL (10,2) NOT NULL
+    , price DECIMAL (10,2) NOT NULL
+    , stock_quantity INT NOT NULL
+    );
